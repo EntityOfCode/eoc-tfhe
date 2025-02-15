4c72cd13ceb8ce9ad79356001536aa55d1fe03fc
Claoo <stirbeiclaudiu@gmail.com>
2025-02-15 06:52:54 +0200

    fix: Update ao-tfhe build script to match ao-llama pattern

--- ao-tfhe/build.sh
+++ ao-tfhe/build.sh
@@ -1,9 +1,7 @@
 
-# Compile with TFHE library
-emcc eoc-tfhe-bindings.c -c -sMEMORY64=1 -Wno-experimental -o eoc-tfhe-bindings.o /lua-5.3.4/src/liblua.a -I/lua-5.3.4/src -I/src/libs/tfhe/include
-emcc eoc-tfhe-run.cpp -c -sMEMORY64=1 -Wno-experimental -o eoc-tfhe-run.o -I/src/libs/tfhe/include
+em++ -sMEMORY64=1 -msimd128 -O3 -fno-rtti -fno-exceptions -Wno-experimental -c eoc-tfhe-run.cpp -o eoc-tfhe-run.o -I/src/libs/tfhe/include -I/lua-5.3.4/src
+emcc -sMEMORY64=1 -msimd128 -O3 -fno-rtti -fno-exceptions -Wno-experimental -c eoc-tfhe-bindings.c -o eoc-tfhe-bindings.o -I/src/libs/tfhe/include -I/lua-5.3.4/src
 
-echo "Linking... eoc-tfhe-bindings.o eoc-tfhe-run.o"
-emcc eoc-tfhe-bindings.o eoc-tfhe-run.o -o libaotfhe.so /src/libs/tfhe/libtfhe.a
+emar rcs libaotfhe.so eoc-tfhe-bindings.o eoc-tfhe-run.o
 
 rm eoc-tfhe-bindings.o eoc-tfhe-run.o
