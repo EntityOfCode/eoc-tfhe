b3c2af11020f6b4ebdc85a2f588a52531f02c2db
Claoo <stirbeiclaudiu@gmail.com>
2025-02-15 06:12:57 +0200

    fix: Update TFHE include paths in ao-tfhe build script

--- ao-tfhe/build.sh
+++ ao-tfhe/build.sh
@@ -1,6 +1,6 @@
 
-emcc eoc-tfhe-bindings.c -c -sMEMORY64=1 -Wno-experimental -o eoc-tfhe-bindings.o /lua-5.3.4/src/liblua.a -I/lua-5.3.4/src -I/tfhe/src/include/
-emcc eoc-tfhe-run.cpp -c -sMEMORY64=1 -Wno-experimental -o eoc-tfhe-run.o -I/tfhe/src/include/
+emcc eoc-tfhe-bindings.c -c -sMEMORY64=1 -Wno-experimental -o eoc-tfhe-bindings.o /lua-5.3.4/src/liblua.a -I/lua-5.3.4/src -I/ao-tfhe/include
+emcc eoc-tfhe-run.cpp -c -sMEMORY64=1 -Wno-experimental -o eoc-tfhe-run.o -I/ao-tfhe/include
 
 echo "Linking... eoc-tfhe-bindings.o eoc-tfhe-run.o"
 emar rcs libaotfhe.so eoc-tfhe-bindings.o eoc-tfhe-run.o
