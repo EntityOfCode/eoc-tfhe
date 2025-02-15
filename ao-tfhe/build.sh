1efc412caca2bf70ff3be409bd63eb56cfb7e1d4
Claoo <stirbeiclaudiu@gmail.com>
2025-02-15 07:00:00 +0200

    fix: Add -mwasm64 flag to compilation commands

--- ao-tfhe/build.sh
+++ ao-tfhe/build.sh
@@ -1,6 +1,6 @@
 
-em++ -sMEMORY64=1 -msimd128 -O3 -fno-rtti -fno-exceptions -Wno-experimental -c eoc-tfhe-run.cpp -o eoc-tfhe-run.o -I/src/libs/tfhe/include -I/lua-5.3.4/src
-emcc -sMEMORY64=1 -msimd128 -O3 -fno-rtti -fno-exceptions -Wno-experimental -c eoc-tfhe-bindings.c -o eoc-tfhe-bindings.o -I/src/libs/tfhe/include -I/lua-5.3.4/src
+em++ -sMEMORY64=1 -msimd128 -mwasm64 -O3 -fno-rtti -fno-exceptions -Wno-experimental -c eoc-tfhe-run.cpp -o eoc-tfhe-run.o -I/src/libs/tfhe/include -I/lua-5.3.4/src
+emcc -sMEMORY64=1 -msimd128 -mwasm64 -O3 -fno-rtti -fno-exceptions -Wno-experimental -c eoc-tfhe-bindings.c -o eoc-tfhe-bindings.o -I/src/libs/tfhe/include -I/lua-5.3.4/src
 
 emar rcs libaotfhe.so eoc-tfhe-bindings.o eoc-tfhe-run.o
 
